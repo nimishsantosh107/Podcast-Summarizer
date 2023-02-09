a612ba3fb2e6342561af9dbb464807034989fdb1
nimishsantosh107 <36289353+nimishsantosh107@users.noreply.github.com>
2023-02-10 02:04:39 +0530

    transcribe & summarize logic finalized

--- src/1_transcribe.py
+++ src/1_transcribe.py
@@ -1,26 +1,31 @@
 import os
 import time
-
 import whisper
 
 PODCAST_ROOT_DIR =  os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tmp_podcasts")
 TRANSCRIPT_ROOT_DIR =  os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tmp_transcripts")
 FILENAME = "000_welcome"
-MODEL = "small.en"
-
-model = whisper.load_model(MODEL, device="cuda")
-
-print("TRANSCRIBING...")
-start_time = time.time()
-result = model.transcribe(os.path.join(PODCAST_ROOT_DIR, FILENAME+".mp3"))
-text = str.strip(result["text"])
-stop_time = time.time()
-print("DONE TRANSCRIBING")
+MODEL = "base.en"
 
 
-print("\n*** STATS ***")
-print("Time (s): \t", stop_time - start_time)
-print("WordCount:\t", len(text.split()))
+if __name__ == "__main__":
+    model = whisper.load_model(MODEL, device="cuda")
 
-with open(os.path.join(TRANSCRIPT_ROOT_DIR, FILENAME+".txt"), 'w') as f:
-    f.write(text)
+    # TIME BLOCK
+    print("TRANSCRIBING...")
+    start_time = time.time()
+
+    result = model.transcribe(os.path.join(PODCAST_ROOT_DIR, FILENAME+".mp3"))
+    text = str.strip(result["text"])
+    
+    stop_time = time.time()
+    print("DONE TRANSCRIBING")
+    # TIME BLOCK
+
+
+    print("\n*** STATS ***")
+    print("Time (s): \t", stop_time - start_time)
+    print("WordCount:\t", len(text.split()))
+
+    with open(os.path.join(TRANSCRIPT_ROOT_DIR, FILENAME+".txt"), 'w') as f:
+        f.write(text)
--- src/2_summarize.py
+++ src/2_summarize.py
@@ -1,15 +1,39 @@
+import os
+import time
 from summarizer import Summarizer
 from summarizer.text_processors.coreference_handler import CoreferenceHandler
 
 
-handler = CoreferenceHandler(spacy_model='en_core_web_md', greedyness=.4)
+TRANSCRIPT_ROOT_DIR =  os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tmp_transcripts")
+SUMMARY_ROOT_DIR =  os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tmp_summaries")
+FILENAME = "000_welcome"
+RATIO = 0.5
 
-body = ""
+if __name__ == "__main__":
+    text_in = ""
+    with open(os.path.join(TRANSCRIPT_ROOT_DIR, FILENAME+".txt")) as f:
+        text_in = f.read()
 
-model = Summarizer(sentence_handler=handler)
-result = model(body, ratio=0.5)
-full = ''.join(result)
+
+    model = Summarizer(
+        sentence_handler=CoreferenceHandler(spacy_model='en_core_web_md', greedyness=.4)
+    )
+
+    # TIME BLOCK
+    print("SUMMARIZING...")
+    start_time = time.time()
+
+    result = model(text_in, ratio=RATIO)
+    text_out = ''.join(result)
+
+    stop_time = time.time()
+    print("DONE SUMMARIZING")
+    # TIME BLOCK
+
+    print("\n*** STATS ***")
+    print("Time (s): \t", stop_time - start_time)
+    print("WordCount:\t", len(text_out.split()))
 
 
-print("\n\n\n\n\n")
-print(full)
+    with open(os.path.join(SUMMARY_ROOT_DIR, FILENAME+".txt"), 'w') as f:
+        f.write(text_out)
