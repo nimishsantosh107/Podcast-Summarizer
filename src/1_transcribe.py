d91d37c50e0a6cce2ce3c719a947d364903f1848
nimishsantosh107 <36289353+nimishsantosh107@users.noreply.github.com>
2023-02-10 01:49:38 +0530

    1_transcribe final logic done

---
+++ src/1_transcribe.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+import whisper
+
+PODCAST_ROOT_DIR =  os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tmp_podcasts")
+TRANSCRIPT_ROOT_DIR =  os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "tmp_transcripts")
+FILENAME = "000_welcome"
+MODEL = "small.en"
+
+model = whisper.load_model(MODEL, device="cuda")
+
+print("TRANSCRIBING...")
+start_time = time.time()
+result = model.transcribe(os.path.join(PODCAST_ROOT_DIR, FILENAME+".mp3"))
+text = str.strip(result["text"])
+stop_time = time.time()
+print("DONE TRANSCRIBING")
+
+
+print("\n*** STATS ***")
+print("Time (s): \t", stop_time - start_time)
+print("WordCount:\t", len(text.split()))
+
+with open(os.path.join(TRANSCRIPT_ROOT_DIR, FILENAME+".txt"), 'w') as f:
+    f.write(text)
